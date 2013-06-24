956847922d48c75597da69b830fecd9ed103262a
Carlos Cuba <ce.cuba@icloud.com>
2013-06-23 23:08:22 -0500

    Site updated at 2013-06-24 04:08:22 UTC

--- javascripts/ajaxify.js
+++ javascripts/ajaxify.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
 						   
 	var hash = window.location.hash.substr(1);
-	var href = $('#nav li#ajax a').each(function(){
+	//alert('hash -> ' + hash);
+	var href = $('#nav li #ajax a').each(function(){
 		var href = $(this).attr('href');
-		if(hash==href.substr(0,href.length-5)){
+		if(hash==href.substr(0,href.length)){
 			var toLoad = hash+'.html #toload';
-			$('#toload').load(toLoad)
+			alert('to load -> ' + toLoad);
+			$('#toload').load(toLoad)	
 		}											
 	});
 
-	$('#nav li#ajax a').click(function(){
+	$('#nav li #ajax a').click(function(){
 								  
 		var toLoad = $(this).attr('href')+' #toload';
 		$('#toload').fadeTo('fast',0.00001,loadContent);
 		$('#load').remove();
-		$('#wrapper').append('<span id="load">LOADING...</span>');
+		$('#wrapper').append('<span id="load">Cargando ...</span>');
 		$('#load').fadeIn('fast');
-		window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);
+		window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length);
+		//alert('window location hash -> ' + window.location.hash);
 		function loadContent() {
 			$('#toload').load(toLoad,showNewContent())
 		}
