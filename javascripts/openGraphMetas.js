e5f7d5ff82ee4c0409295df30c06d6a440874bef
Carlos Cuba <ce.cuba@icloud.com>
2013-06-24 22:07:21 -0500

    Site updated at 2013-06-25 03:07:21 UTC

--- javascripts/openGraphMetas.js
+++ javascripts/openGraphMetas.js
@@ -9,8 +9,8 @@ Description:
 $(document).ready(function() {
 	var metaImgContent = $('#imageMeta').html().replace('"','');
 	var metaImg = '<meta property="og:image" content="'+ metaImgContent +'" />'
-	var metaDescContent = $('#descMeta').html();
-	var metaDesc = '<meta property="og:description" content="'+ metaDescContent +'" />'
 	$('head').prepend(metaImg);
-	$('head').prepend(metaDesc);
+
+	var metaDescContent = $('#descMeta').html();
+	$('meta[name=description]').attr("content", metaDescContent);
 });
